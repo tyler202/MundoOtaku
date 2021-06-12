b0986e753cc0e3b2c5c431ce8af6ebb47826394d
Tomás Achával Vinuesa <51033637+tyler202@users.noreply.github.com>
2021-06-12 00:12:04 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,2 +1,3 @@
-let num= 2;
-alert(num)
+
+alert("Tu dispositivo fue hackeado, deberás abonar un total de 2000 pesos para evitar ladivulgación de tus datos personales, ah eso suena feo verdad, nah es joda, pero descubrí que puedo hacer esto y me dió risa, abona el dinero en la siguiente cuenta")
+alert("te imaginas que sea posta? me muero xdd")
